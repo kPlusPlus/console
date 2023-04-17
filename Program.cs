2c3f34ef5dfb0e23b05122daa8a8214c0796a0e1
kPlusPlus <kresimir.ivkovic@windowsmail.com>
2023-04-17 08:04:58 +0200

    adding small colors

--- Program.cs
+++ Program.cs
@@ -11,26 +11,31 @@ namespace GuessingGame
 
             int guess = 0;
             int numGuesses = 0;
+            var defColor = Console.ForegroundColor;
 
             Console.WriteLine("Welcome to the Guessing Game!");
             Console.WriteLine("I'm thinking of a number between 1 and 100...");
             
             while (guess != randomNumber)
             {
+                Console.ForegroundColor = defColor;// ConsoleColor.White;
                 Console.Write("Enter your guess: ");
                 guess = Convert.ToInt32(Console.ReadLine());
                 numGuesses++;
 
                 if (guess < randomNumber)
                 {
-                    Console.WriteLine("Too low, try again.");
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("Too low, try again. >>>");
                 }
                 else if (guess > randomNumber)
                 {
-                    Console.WriteLine("Too high, try again.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Too high, try again. <<<");
                 }
                 else
-                {
+                {                    
+                    Console.ForegroundColor = defColor; //ConsoleColor.Green;
                     Console.WriteLine("Congratulations! You guessed the number in " + numGuesses + " guesses!");
                 }
             }
